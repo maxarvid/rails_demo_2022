54aa0ad37051bd1894e4ece589622a83aa90f1ce
Thomas Ochman <thomas@dragster.se>
2022-02-22 11:13:12 +0100

    adds query to articles#index

--- spec/requests/api/articles/index_spec.rb
+++ spec/requests/api/articles/index_spec.rb
@@ -14,6 +14,8 @@ RSpec.describe 'GET /api/articles', type: :request do
     expect(subject.status).to eq 200
   end
 
+  it { is_expected.to have_http_status 200 }
+
   it 'is expected to return a collection of 5 articles' do
     expect(response_json['articles'].size).to eq 5
   end
