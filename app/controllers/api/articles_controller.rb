ee4a94d65e3b05db49bc4ba5f529679c056db6d6
Thomas Ochman <thomas@dragster.se>
2022-02-23 10:36:11 +0100

    adds create action to articles_controller adds spec for article create

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,18 +1,21 @@
 class Api::ArticlesController < ApplicationController
   def index
-    # binding.pry
     articles = Article.all
     render json: { articles: articles }
   end
 
   def show
     article = Article.find(params[:id])
-    render json: { article: article }
+    render json: { article: article } 
   end
 
-  # def create
-
-  # end
+  def create
+    new_article = Article.new
+    new_article.title = params[:article][:title]
+    new_article.body = params[:article][:body]
+    new_article.save
+    render json: { article: new_article }, status: 201
+  end
 
   # def update
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :pings, only: [:index]
-    resources :articles, only: [:index, :show]
+    resources :articles, only: [:index, :show, :create]
     # get :articles, controller: :articles, action: :index
     # get :articles, controller: :articles, action: :show
   end
