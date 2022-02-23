1d10ef97c17c59cddf8e2a9bda60cf94f6d15627
Thomas Ochman <thomas@dragster.se>
2022-02-23 09:58:19 +0100

    adds spec for article body in response

--- spec/requests/api/articles/show_spec.rb
+++ spec/requests/api/articles/show_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'GET /api/articles/:id' do
   # ARRANGE
-  let(:article) { create(:article, title: 'Article 1') }
+  let(:article) { create(:article, title: 'Article 1', body: "My text") }
   let!(:another_article) { create(:article, title: 'Article 2') }
   # ACT
   before do
@@ -12,12 +12,12 @@ RSpec.describe 'GET /api/articles/:id' do
   # ASSERT
   it { is_expected.to have_http_status 200 }
 
-  it 'is expected to return 1 article' do
-    expect(response_json.size).to eq 1
-  end
-
   it 'is expected to include article title' do
     expect(response_json['article']['title']).to eq 'Article 1'
+  end
+
+  it 'is expected to include article body' do
+    expect(response_json['article']['body']).to eq 'My text'
   end
 
   it 'is expected to include the right article title' do
