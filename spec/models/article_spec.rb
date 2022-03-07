f44f95c01706e8300c65119325700dcf48795b36
maxarvid <max.arvid.anderson@gmail.com>
2022-03-07 11:57:40 +0100

    adds active storage, passes article model spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,17 @@ RSpec.describe Article, type: :model do
     it { is_expected.to have_db_column(:body).of_type(:text) }
   end
 
+  describe 'Attachment' do
+    it 'is expected to be valid' do
+      subject.image.attach(
+        io: File.open(fixture_path + '/fixture_image.png'),
+        filename: 'attachment.png',
+        content_type: 'image/png'
+      )
+      expect(subject.image).to be_attached
+    end
+  end
+
   describe 'Factory' do
     it 'is expected to be valid' do
       expect(create(:article)).to be_valid
